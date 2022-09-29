9d6f9ae59ee1678105de3f2dff2376aefc046630
Baxsh_cw07 <Baxsh_cw07@STHQ0127-04>
2022-09-29 16:21:03 +0400

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,21 @@
+#include <conio.h>
+#include <iomanip>
+#include <iostream>
+#include<fcntl.h>
+#include<stdio.h>
+#include<Windows.h>
+#include<time.h>
+using namespace std;
+
+#include"struct.h"
+#include"function.h"
+
+
+void main() {
+
+	srand(time(0));
+
+	//FirstScreen();
+	ShowMenu();
+
+}
---
+++ Project1/struct.h
@@ -0,0 +1,26 @@
+#pragma once
+
+enum Simvol { UP = 72, DOWN = 80, RIGHT = 77, LEFT = 75, ENTER = 13, REVERS = 113 };
+
+struct Pacman {
+
+	int x = 5;
+	int y = 0;
+
+}pacman;
+
+struct Striked
+{
+	bool striked = false;
+	bool right = false;
+	bool right_Access = false;
+	bool left = false;
+	bool left_Access = false;
+	bool up = false;
+	bool up_Access = false;
+	bool dowm = false;
+	bool dowm_Access = false;
+	int striked_x = -1;
+	int striked_y = -1;
+
+}strike;
